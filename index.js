32a070051697b12fa0f00610bab3d248ceba37a7
Tushar <tushard3998@gmail.com>
2024-02-08 12:26:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log("JavaScript Functions")
+
+/*
+
+NORMAL FUNCTIONS
+
+function nice(name){
+  console.log("Hey " + name + " you are nice!!")
+  console.log("Hey " + name + " You Are Good!!")
+  console.log("Hey " + name + " You Are Very Good!!")
+  console.log("Hey " + name + " You Are OK!!")
+}
+
+nice("Tushar")
+
+//PROPER FUCTION SYNTAX
+
+function sum(a, b ,c) {
+    return a+b+c
+}
+
+result = sum(3,5,7)
+result1 = sum(3,6,7)
+result2 = sum(3,7,7)
+result3 = sum(3,8,7)
+
+console.log("The sum of this numbers is: " + result)
+console.log("The sum of this numbers is: " + result1)
+console.log("The sum of this numbers is: " + result2)
+console.log("The sum of this numbers is: " + result3)+*/
+
+//ARROW FUNCTION
+
+const func1 = (x)=>{
+    console.log("I am an arrow function ",x)
+}
+
+func1(34);
+func1(35)
+func1(36);
